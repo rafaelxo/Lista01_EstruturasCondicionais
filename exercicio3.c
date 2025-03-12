ecc6025a0d982b91bcb1aee6ddb28023ae9b18c4
rafaelxo <rafaelxo2007@gmail.com>
2025-03-12 15:10:38 -0300

    Add files via upload

--- exercicio3.c
+++ exercicio3.c
@@ -3,10 +3,17 @@
 #include <math.h>
 
 int main() {
-    int ano_nascimento, ano_atual = 2025, idade;
+    int ano_nascimento, ano_atual, idade;
     char aniversario;
-    scanf("%d %c", &ano_nascimento, &aniversario);
+    scanf("%d", &ano_nascimento);
+    scanf("%d", &ano_atual);
+    scanf(" %c", &aniversario);
     idade = ano_atual - ano_nascimento;
+    if (aniversario == 'S') {
+        idade = idade;
+    } else if (aniversario == 'N') {
+        idade = idade - 1;
+    }
     if (idade <= 17) {
         printf("%d\nNao pode dirigir", idade);
     } else if (idade >= 18) {
